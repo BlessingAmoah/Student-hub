b2c3bb13a9ca39572ec7c8d077f2e40b29606b1b
Blessing Amoah <blessing@meta.com>
2024-07-24 17:45:09 -0700

    Created a notification drawer which display all unread notifications.

---
+++ Frontend/student-hub/src/components/NotificationDrawer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Drawer, List, ListItem, ListItemText, IconButton } from '@mui/material';
+import { Close as CloseIcon } from '@mui/icons-material';
+
+const NotificationDrawer = ({ open, onClose, notifications }) => (
+  <Drawer anchor="right" open={open} onClose={onClose}>
+    <IconButton onClick={onClose} style={{ position: 'absolute', top: 10, right: 10 }}>
+      <CloseIcon />
+    </IconButton>
+    <List>
+      {notifications.map(notification => (
+        <ListItem key={notification.id}>
+          <ListItemText primary={notification.message} />
+        </ListItem>
+      ))}
+    </List>
+  </Drawer>
+);
+
+export default NotificationDrawer;
