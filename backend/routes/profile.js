db651501d371f4e596276d3d95db25080fcbb5e4
Blessing Amoah <bsa5@calvin.edu>
2024-08-11 17:42:40 -0700

    updated dependencies and added r2 cloudstorage

--- backend/routes/profile.js
+++ backend/routes/profile.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { User } = require('../models');
 const router = express.Router();
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
+const upload = require('../server').upload; 
 const verifyToken = require('../middleware/auth');
 const axios = require('axios')
 
