d89163e052a2af8f99136f63049dca0598d8589b
Blessing Amoah <bsa5@calvin.edu>
2024-08-13 11:43:12 -0700

    updated to add auto to the region

--- backend/config/r2Config.js
+++ backend/config/r2Config.js
@@ -4,6 +4,7 @@ const r2 = new AWS.S3({
   endpoint: process.env.CLOUDFLARE_R2_ENDPOINT, 
   accessKeyId: process.env.CLOUDFLARE_R2_ACCESS_KEY_ID,
   secretAccessKey: process.env.CLOUDFLARE_R2_SECRET_ACCESS_KEY,
+  region: 'auto',
   signatureVersion: 'v4',
 });
 
