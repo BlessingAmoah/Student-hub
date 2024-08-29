99b33fe763b954aa8d746057c0819e7ff4470829
Blessing Amoah <bsa5@calvin.edu>
2024-08-28 22:18:54 -0400

    updated the expired database

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,27 +1,25 @@
 require('dotenv').config();
 
-
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME,
+    username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
   },
   test: {
-    username: process.env.DB_USERNAME,
+    username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
   },
   production: {
-    username: process.env.DB_USERNAME,
+    username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
-    use_env_variable: 'DATABASE_HOSTED_URL',
   },
 };
