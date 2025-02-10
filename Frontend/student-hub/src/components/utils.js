e92922f0688b5013c14e34903a23230f67b689fd
Blessing Amoah <bsa5@calvin.edu>
2025-02-09 23:10:35 -0500

    remove unused files and configurations from backend and frontend

--- Frontend/student-hub/src/components/utils.js
+++
@@ -1,9 +0,0 @@
-
- // util function to fetch userId & token
- const getUserIDToken = () => {
-    const userId = sessionStorage.getItem('userId');
-    const token = sessionStorage.getItem('token');
-    return { userId, token };
-}
-
-export default getUserIDToken;
--- backend/config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    DATABASE_HOSTED_URL: process.env.DATABASE_HOSTED_URL
-  };
